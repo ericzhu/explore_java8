62b5e0844be4bdb0d978bad90fac1161239c450c
eric.zhu <eric.zhu@kronos.com>
2015-06-29 16:36:47 -0400

    new controls and panes

---
+++ learn_javafx/src/main/java/learn/javafx/ch10/pane/BorderPaneTest.java
@@ -0,0 +1,17 @@
+package learn.javafx.ch10.pane;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BorderPaneTest extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+	}
+
+}
